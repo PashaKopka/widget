fa3554e28024d2d382f216258a1c98312fd29b8a
PashaKopka <pashakopka216@gmail.com>
2020-11-08 19:29:54 +0200

    window visible at all v-descktops, refactoring, adding some docs and ui changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import time
 
+import pyvda
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import QTime, QTimer, Qt
 from ui.mydesign import Ui_Form
 from PyQt5 import QtWidgets, uic
 import sys
+import win32gui
 
 MAIN_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
@@ -18,20 +20,29 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.label.mouseMoveEvent = self._move_window
-
         self._normalize_window()
-
         self.ui.label.setText(self._get_time())
-
         self._set_timer(method=self._change_label_time, timeout=1000)
 
+        self._stay_at_all_virtual_descktops()
+
     def _move_window(self, e):
+        """
+        Function for moving window
+        :param e: -
+        :return: None
+        """
         if not self.isMaximized() and e.buttons() == Qt.LeftButton:
             self.move(self.pos() + e.globalPos() - self.click_position)
             self.click_position = e.globalPos()
             e.accept()
 
     def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        """
+        Function for handling mouse click event
+        :param a0:
+        :return: None
+        """
         self.click_position = a0.globalPos()
 
     def _set_timer(self, method, timeout):
@@ -59,11 +70,22 @@ class MyWindow(QtWidgets.QMainWindow):
         and set window always on bottom
         :return: None
         """
-        self.setWindowFlags(
-            QtCore.Qt.FramelessWindowHint |
-            QtCore.Qt.WindowStaysOnBottomHint
-        )
+        flags = QtCore.Qt.FramelessWindowHint
+        flags |= QtCore.Qt.Tool
+        flags |= QtCore.Qt.WindowStaysOnBottomHint
+        self.setWindowFlags(flags)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+    @staticmethod
+    def _stay_at_all_virtual_descktops():
+        """
+        Function make window visible at all virtual desktops
+        (tested only on windows 10)
+        :return:
+        """
+        current_desktop = pyvda.GetCurrentDesktopNumber()
+        current_window_handle = win32gui.GetForegroundWindow()
+        pyvda.MoveWindowToDesktopNumber(current_window_handle, current_desktop)
 
     @staticmethod
     def _get_time():
--- ui/mydesign.py
+++ ui/mydesign.py
@@ -14,10 +14,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(358, 170)
+        Form.resize(454, 180)
         Form.setStyleSheet("background-color: transparent;")
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(50, 30, 271, 91))
+        self.label.setGeometry(QtCore.QRect(50, 30, 371, 101))
         font = QtGui.QFont()
         font.setPointSize(50)
         self.label.setFont(font)
